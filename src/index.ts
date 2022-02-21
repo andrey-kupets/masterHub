be948c06132a4310ea09078886b184a96bcd48d8
andrey-kupets <andrey.kupets@i.ua>
2022-02-21 23:59:49 +0200

    add main scripts & tsconfig.json

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,22 @@ import { app } from './app';
 
 const server = http.createServer(app);
 
-server.listen(5000);
+server.listen(5000, () => {
+  console.log('5000th PORT is being listened');
+});
+
+process.on('uncaughtException', error => {
+  console.log(error);
+  server.close(() => {
+    process.exit();
+  })
+});
+
+process.on('unhandledRejection', error => {
+  console.log(error);
+  server.close(() => {
+    process.exit();
+  })
+});
 
 
