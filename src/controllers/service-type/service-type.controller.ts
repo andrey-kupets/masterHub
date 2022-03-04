c7d0cb91cb2dd89cf76e4034a4f2db77ca50377c
andrey-kupets <andrey.kupets@i.ua>
2022-03-04 14:33:13 +0200

    fix adding created service-type element to service.type-field at service-model

--- src/controllers/service-type/service-type.controller.ts
+++ src/controllers/service-type/service-type.controller.ts
@@ -1,15 +1,23 @@
 import { NextFunction, Request, Response } from 'express';
-import { ServiceTypeModel } from '../../schemas';
+import { ServiceModel, ServiceTypeModel } from '../../schemas';
 
 class ServiceTypeController {
-  async createServiceType(req: Request, res: Response, next: NextFunction) {
+  async createServiceType(req: Request, res: Response, next: NextFunction): Promise<void> {
     try {
-      await ServiceTypeModel.create({
-        service_id: req.params.service_id,
-        ...req.body
+      const { body, params: { service_id } } = req;
+
+      const createdServiceType = await ServiceTypeModel.create({
+        service_id,
+        ...body
       });
 
-      res.json('OK');
+      await ServiceModel.findOneAndUpdate(
+        service_id as any, // or use findByIdAndUpdate w/o "as any"
+        {
+        $addToSet: { type: createdServiceType._id }
+      })
+
+      res.json(createdServiceType);
     } catch (e) {
       next(e);
     }
