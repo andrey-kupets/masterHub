59052e17b123e9fc21e92132703768acb09028ad
andrey-kupets <andrey.kupets@i.ua>
2022-03-05 12:10:00 +0200

    test errorHandler

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import * as cors from 'cors';
 import * as express from 'express';
+import { Request, Response, NextFunction } from 'express';
 import * as mongoose from 'mongoose';
 import * as morgan from 'morgan';
 import helmet from 'helmet';
@@ -19,7 +20,9 @@ class App {
     this.app.use(morgan('dev'));
 
     this.mountRoutes();
-    this.setupDB();
+    this.setupDB()
+
+    this.app.use(this.mainErrorHandler);
   }
 
   private configureCors(origin: any, callback: any) {
@@ -53,7 +56,13 @@ class App {
     this.app.use('/service-types', serviceTypeRouter);
   }
 
-
+  private mainErrorHandler(err: Error | any, req: Request, res: Response, next: NextFunction) {
+    res
+      .status(err.status | 500)
+      .json({
+        message: err.message || 'Server error'
+      })
+  }
 }
 
 export const app = new App().app;
